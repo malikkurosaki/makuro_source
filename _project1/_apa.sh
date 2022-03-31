45885ea35b48b51c67df96de395b09a286a81ea9
malikkurosaki <kurosakiblackangel@gmail.com>
2022-03-31 14:22:44 +0800

    ya

--- _project1/_apa.sh
+++ _project1/_apa.sh
@@ -1,3 +1,3 @@
 _apa(){
-    echo "ini adalah dari apa"
+    pwd
 }
--- project.sh
+++ project.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 . <(curl -s "https://raw.githubusercontent.com/malikkurosaki/makuro_source/main/_project1/autoload.sh")
